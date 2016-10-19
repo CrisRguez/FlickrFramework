f5ea339025b8702507d5603441d0b921a696a393
Cristina <usuario@cristina-mac.local>
2016-10-19 23:32:38 +0200

    Initial Commit

---
+++ FlickrFramework/FlickrFramework.h
@@ -0,0 +1,19 @@
+//
+//  FlickrFramework.h
+//  FlickrFramework
+//
+//  Created by Cristina on 19/10/16.
+//  Copyright © 2016 Cristina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlickrFramework.
+FOUNDATION_EXPORT double FlickrFrameworkVersionNumber;
+
+//! Project version string for FlickrFramework.
+FOUNDATION_EXPORT const unsigned char FlickrFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlickrFramework/PublicHeader.h>
+
+
